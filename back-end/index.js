d8d36689ff3f7c2972da8a84a0b8789a9e7cb3aa
Zhang, Zhen <41971724+Jeffzholy@users.noreply.github.com>
2019-11-15 09:48:54 +0800

    JSAE-10: Setup linter & commit hook (#6)
    * JSAE-63 & JSAE-64: Add eslint and prettier
    
    * JSAE-65: Add git hook to lint before commit
    
    * JSAE-65: Modify the pre-commit hook and lint the code
    
    * JSAE-65: Modify the pre-commit hook:
    --make it executed before commit

--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,22 @@
-require("dotenv").config();
-const PORT = process.env.PORT || 3000;
-const express = require("express");
-const cors = require("cors");
-const app = express();
+require('dotenv').config()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000
+const express = require('express')
+const cors = require('cors')
 
-app.get("/", (req, res) => {
-  res.end("setup");
-});
+const app = express()
 
-app.get("*", (req, res) => res.send("Page Not found 404"));
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+console.log('ffg')
+
+app.get('/', (req, res) => {
+  res.end('setup')
+})
+
+app.get('*', (req, res) => res.send('Page Not found 404'))
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+  console.log(`Server is running on ${PORT}`)
+})
