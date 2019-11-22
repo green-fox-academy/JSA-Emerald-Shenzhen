2796c851d494c06f2b6eddb1424ff34abbb26769
Zhang, Zhen <41971724+Jeffzholy@users.noreply.github.com>
2019-11-22 14:08:28 +0800

    JSAE-74: Configure redux for the project (#25)
    * JSAE-74: Install redux redux-thunk & react-redux
    
    * JSAE-74: Set up redux
    
    * JSAE-74: Configure devTools in redux & wrap AppLoading with store
    
    * JSAE-74: Uppercase thunk in store file

--- front-end/App.js
+++ front-end/App.js
@@ -5,12 +5,14 @@ import Roboto from 'native-base/Fonts/Roboto.ttf'
 import RobotoMedium from 'native-base/Fonts/Roboto_medium.ttf'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { Provider } from 'react-redux'
 
 import LoanList from './src/components/LoanList/LoanList'
 import NewLoan from './src/components/NewLoan/NewLoan'
 import MainPage from './src/components/MainPage/MainPage'
 
 import useFonts from './src/hooks/loadFonts'
+import store from './src/lib/store'
 
 const AppStack = createStackNavigator(
   {
@@ -36,5 +38,13 @@ const AppContainer = createAppContainer(AppStack)
 
 export default function App() {
   const isReady = useFonts({ Roboto, RobotoMedium, ...Ionicons.font })
-  return isReady ? <AppContainer /> : <AppLoading />
+  return isReady ? (
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+  ) : (
+    <Provider store={store}>
+      <AppLoading />
+    </Provider>
+  )
 }
---
+++ front-end/src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+const initState = {}
+
+// eslint-disable-next-line no-unused-vars
+const rootReducer = (state = initState, action) => {
+  return state
+}
+
+export default rootReducer
