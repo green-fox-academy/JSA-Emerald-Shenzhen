0b510ad1bca9657cb5f92212f912e5908a91dacb
Garrin Wang <Garrin_Wang@epam.com>
2019-12-02 17:33:09 +0800

    added redux for products with test

--- front-end/tests/components/ProductCard.test.js
+++ front-end/tests/components/ProductCard.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import ProductCard from '../../src/components/ProductSelection/ProductCard'
-import data from '../../helpers/fakeProductsData'
+import data from '../../helpers/mockData_FE'
 
 describe('<ProductCard />', () => {
   it('<ProductCard > has 2 child', () => {
